e127e54232a626ab1d12eae02d1cbfa5b547ffa1
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-02-15 20:44:32 +0530

    image error fix

--- src/components/LinksButton.jsx
+++ src/components/LinksButton.jsx
@@ -1,10 +1,12 @@
 import "../css/LinksButton.css";
+import githubLogo from "../assets/github.png";
+import linkLogo from "../assets/link.png";
 
 const LinksButton = ({ type, link }) => {
   return (
     <button className="link-button">
       <a href={link} target="_blank" rel="noreferrer">
-        <img src={`../src/assets/${type}.png`} alt={`${type}`} />
+        <img src={type === "github" ? githubLogo : linkLogo} alt={`${type}`} />
       </a>
     </button>
   );
