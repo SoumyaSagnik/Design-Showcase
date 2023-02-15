9eee96d89e7008ce52515a095601c1bd35419246
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-02-15 20:25:14 +0530

    img location fix

--- src/components/LinksButton.jsx
+++ src/components/LinksButton.jsx
@@ -4,7 +4,7 @@ const LinksButton = ({ type, link }) => {
   return (
     <button className="link-button">
       <a href={link} target="_blank" rel="noreferrer">
-        <img src={`/src/assets/${type}.png`} alt={`${type}`} />
+        <img src={`../src/assets/${type}.png`} alt={`${type}`} />
       </a>
     </button>
   );
